068f7b6e0d3dd9a4a349fec8f6aaff5d4073f4fa
Advait Patil <advaitpatil2@gmail.com>
2026-06-13 19:13:50 -0400

    fixed CORS typo

--- backend/main.py
+++ backend/main.py
@@ -14,7 +14,7 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         "https://www.nflcortisol.com",
-        "https://www.nflcortisol.com,"
+        "https://nflcortisol.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
