f724c3d0a6f220a964e697385a7d4cd92f54786a
Advait Patil <advaitpatil2@gmail.com>
2026-06-13 19:07:07 -0400

    fixed CORS issue again

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://www.nflcortisol.com"
+        "https://www.nflcortisol.com",
+        "https://www.nflcortisol.com,"
     ],
     allow_credentials=True,
     allow_methods=["*"],
